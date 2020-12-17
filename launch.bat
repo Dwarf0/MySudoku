6dda24ba30474d9c3fb2cd76857deb774556a74d
Dwarf0 <maxime42300@hotmail.fr>
2020-12-17 23:11:32 +0100

    Correction de la gestion de la dépendance UI/Solver

--- launch.bat
+++ launch.bat
@@ -4,7 +4,7 @@ call winEnv.bat
 mkdir _build
 cd _build
 
-set PATH=%PATH%;%QT_DIRS%
+set PATH=%PATH%;%QT_DIRS%;%OUTPUT_DIR%/SudokuSolver/Debug;%OUTPUT_DIR%/SudokuSolver/Release
 
 %CMAKE_EXE% .. -G %VISUAL_VERSION% -DGTEST_ROOT:STRING=%GTESTLIB_DIR% -DCMAKE_INSTALL_PREFIX=%OUTPUT_DIR%
 
